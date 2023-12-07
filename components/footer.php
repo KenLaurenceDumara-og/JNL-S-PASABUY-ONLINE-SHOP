2933b84b04a2385e6680ef6dadba151f38cd57ea
KenLaurenceDumara-og <113278468+KenLaurenceDumara-og@users.noreply.github.com>
2023-12-07 23:54:32 +0800

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,40 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> HOME</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> ABOUT</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> SHOP</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> CONTACT</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> cart</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> orders</a>
+      </div>
+
+      <div class="box">
+         <h3>contact us</h3>
+         <a href="ken@gmail.com"><i class="fas fa-phone"></i> 0993823882</a>
+         <a href="tel:0998387328"><i class="fas fa-phone"></i> +6398857885</a>
+         <a href="https://mail.google.com/"><i class="fas fa-envelope"></i> JNLSPASABUY@gmail.com</a>
+         <a href="https://www.google.com/myplace"><i class="fas fa-map-marker-alt"></i> San Pascual, Batangas </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="https://www.facebook.com/JNLsPasabuy"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+         
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>mr. web designer</span> | all rights reserved!</div>
+
+</footer>
